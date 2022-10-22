a28b8e324768e429e2c11667ca21cc0909047829
Ariel Rin <joel.falknau@gmail.com>
2022-10-22 14:50:44 +1000

    update setup.py to current deps and classifiers

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from setuptools import find_packages, setup
 
 from signalpings import __version__
@@ -15,20 +15,31 @@ setup(
     license='MIT',
     description='Pings for Signals!',
     long_description=README,
-    long_description_content_type='text/markdown', 
+    long_description_content_type='text/markdown',
     url='https://github.com/pvyParts/allianceauth-signal-pings',
     author='Aaron Kable',
     author_email='aaronkable@gmail.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ]
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "allianceauth>=2.15.1,<4.0.0",
+    ],
 )
+
+
